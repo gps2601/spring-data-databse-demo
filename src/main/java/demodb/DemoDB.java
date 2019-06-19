55afc0d654ad85de2b3d2d24386b18f7451d9569
Geoff Smith <smith260194@gmail.com>
2019-06-19 10:14:46 +0100

    Adding some test stuff

--- src/main/java/demodb/DemoDB.java
+++ src/main/java/demodb/DemoDB.java
@@ -1,4 +1,12 @@
 package demodb;
 
 public class DemoDB {
+    public String sayHello(){
+
+        return hmm();
+    }
+
+    private String hmm() {
+        return "test";
+    }
 }
--- src/test/java/demodb/DemoDBTest.java
+++ src/test/java/demodb/DemoDBTest.java
@@ -7,8 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DemoDBTest {
     @Test
     public void testAdd() {
-
-
         assertEquals(42, Integer.sum(19, 23));
+    }
+
+    @Test
+    public void saysHello() {
+        DemoDB demoDB = new DemoDB();
+
+        assertEquals(demoDB.sayHello(), "Hello");
     }
 }
